2c258307759d46e9032ce5b36a3e5927b64be274
nathan <nathanhalioua@gmail.com>
2023-03-13 12:23:50 +0100

    Oui

--- src/Tests/GestionTourneeTest.java
+++ src/Tests/GestionTourneeTest.java
@@ -11,13 +11,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GestionTourneeTest {
     public Visite maVisite;
+    public Visite maVisite2;
     public GestionTournee gestionTournee;
     public TypePrestation typePrestation;
+    TypePrestation typePrestation2;
     @BeforeEach
     void setUp() {
         Adherent ad1 = new Adherent("Jean", "Jacques", "02561258", "147", "852");
         typePrestation = new TypePrestation(25, "Planter");
         PrestationVisite presta = new PrestationVisite(typePrestation, 4);
+
+        Adherent ad2 = new Adherent("joe", "jie", "02561258", "147", "852");
+        typePrestation2 = new TypePrestation(20, "Creuser");
+        PrestationVisite presta2 = new PrestationVisite(typePrestation2, 2);
+
+        maVisite2 = new Visite(ad2, "18:00");
+        maVisite2.ajouterPrestationVisite(presta2);
 
         maVisite = new Visite(ad1, "14:00");
         maVisite.ajouterPrestationVisite(presta);
@@ -26,16 +35,17 @@ class GestionTourneeTest {
         Date date = new Date();
         Tournee tournee = new Tournee(date,14, ins1);
         tournee.ajouterVisite(maVisite);
+        tournee.ajouterVisite(maVisite2);
         gestionTournee = new GestionTournee(tournee);
     }
 
     @Test
     void CATournee() {
-        assertEquals(100, gestionTournee.CATournee());
+        assertEquals(140, gestionTournee.CATournee());
     }
 
     @Test
     void CATourneeTypePrestation() {
-        assertEquals(100, gestionTournee.CATourneeTypePrestation(typePrestation));
+        assertEquals(40, gestionTournee.CATourneeTypePrestation(typePrestation2));
     }
 }
